5883cac124cc1e542e05ba2317d57bc8e0e0cc65
Nikolay Dachev <ndachev@vmware.com>
2016-01-15 17:59:16 +0200

    pyautogui test

--- autologin.py
+++ autologin.py
@@ -4,4 +4,8 @@ import sys
 sys.path.insert(0, r'./libs')
 import pyautogui
 
+
+# http://rosettacode.org/wiki/Simulate_input/Keyboard#Python
+
 pyautogui.typewrite('Hello world!')
+pyautogui.press('enter')  # press the Enter key
